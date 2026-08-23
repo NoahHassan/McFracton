855830063a73ac1feec6c710e517b736b0225416
Noah Hassan <noahhassan@outlook.de>
2026-08-23 21:05:41 +0200

    supposed claude-fix, still need to check this

--- McFracton/core/src/Spiderweb.cpp
+++ McFracton/core/src/Spiderweb.cpp
@@ -1,4 +1,4 @@
-#include "Spiderweb.h"
+ï»¿#include "Spiderweb.h"
 
 // A fields are treated as:
 // 0: A0
@@ -31,14 +31,15 @@ double Spiderweb::getEnergy() const
 	// treat U as inverse temperature, such that T --> infty will make magnetic fluxes proliferate
 	// Hence H = 1/2(KU) cos(Q_ij A_0 - D0 A_ij) - 1/2 cos(Q_ij A_ij)
 
-	// Since the hamiltonian should be E² + B² and E² = Ex² + Ey² etc. Its probably
+	// Since the hamiltonian should be E^2 + B^2 and E^2 = Ex^2 + Ey^2 etc. Its probably
 	// cos(Q_xx A0 - D0 A_xx) + cos(Q_xy A0 - D0 A_xy)
 	double energy = 0.0;
 	for (int n_site = 0; n_site < nSites; n_site++)
 	{
-		auto e_terms_xx = getElectricTerms_xx(n_site);	// should return vector of (field_index, factor) pairs such that factor * site_fields[field_index]
-		auto e_terms_xy = getElectricTerms_xy(n_site);	// is a term in the cosine of the hamiltonian
-		auto b_terms = getMagneticTerms(n_site);
+		int anchor = n_site * 3;	// any field index at this site works; the term generators only use field_index/3
+		auto e_terms_xx = getElectricTerms_xx(anchor);	// should return vector of (field_index, factor) pairs such that factor * site_fields[field_index]
+		auto e_terms_xy = getElectricTerms_xy(anchor);	// is a term in the cosine of the hamiltonian
+		auto b_terms = getMagneticTerms(anchor);
 
 		double e_sum_xx = 0.0;
 		for (auto term : e_terms_xx)
@@ -65,51 +66,105 @@ double Spiderweb::getEnergy() const
 
 double Spiderweb::proposeSiteFlip(int index, double angle) const
 {
+	// IMPORTANT: because Q_ij involves second derivatives (D_x^2, D_y^2, 4 D_xD_y),
+	// a given field does NOT only appear in the electric/magnetic term centered at
+	// its own site r. It also appears in the terms centered at several NEIGHBORING
+	// sites (the same way a term like (D_x^2 f)(s) = f(s+2x)-2f(s+x)+f(s) means that
+	// changing f(r) affects the term evaluated at s=r, s=r-x AND s=r-2x).
+	// Below we enumerate every (site, term-type) pair whose cosine argument actually
+	// contains site_fields[index], and accumulate cos(new)-cos(old) for each.
+
 	double d_energy = 0.0;
+	int type = index % 3;
+	int site_index = index / 3;
+	const auto site_vector = index_from_site(site_index);
+	int nx = site_vector[0];
+	int ny = site_vector[1];
+	int nt = site_vector[2];
 
-	auto e_terms_xx = getElectricTerms_xx(index);
-	auto e_terms_xy = getElectricTerms_xy(index);
-	double e_sum_old = 0.0;
-	double e_sum_new = 0.0;
-	for (auto term : e_terms_xx)
-	{
-		e_sum_old += term.second * site_fields[term.first];
-		e_sum_new += term.second * site_fields[term.first];
+	int nx_m1 = (nx - 1 + linear_size) % linear_size;
+	int nx_m2 = (nx - 2 + linear_size) % linear_size;
+	int ny_m1 = (ny - 1 + linear_size) % linear_size;
+	int ny_m2 = (ny - 2 + linear_size) % linear_size;
+	int nt_m1 = (nt - 1 + temporal_size) % temporal_size;
 
-		if (term.first == index)
-			e_sum_new += term.second * angle;
-	}
-	d_energy += (cos(e_sum_new) - cos(e_sum_old)) / (2.0 * KU);
-
-	for (auto term : e_terms_xy)
-	{
-		e_sum_old += term.second * site_fields[term.first];
-		e_sum_new += term.second * site_fields[term.first];
-
-		if (term.first == index)
-			e_sum_new += term.second * angle;
-	}
-	d_energy += (cos(e_sum_new) - cos(e_sum_old)) / (2.0 * KU);
-
-	if (index % 3 == 0) // if the type is A0, magnetic term doesnt contribute
-		return d_energy;
-	else 
-	{
-		auto b_terms = getMagneticTerms(index);
-		double b_sum_old = 0.0;
-		double b_sum_new = 0.0;
-		for (auto term : b_terms)
+	auto accumulate_xx = [&](int anchor_field_index, double prefactor)
 		{
-			b_sum_old += term.second * site_fields[term.first];
-			b_sum_new += term.second * site_fields[term.first];
+			auto terms = getElectricTerms_xx(anchor_field_index);
+			double old_sum = 0.0, new_sum = 0.0;
+			for (auto term : terms)
+			{
+				old_sum += term.second * site_fields[term.first];
+				new_sum += term.second * site_fields[term.first];
+				if (term.first == index)
+					new_sum += term.second * angle;
+			}
+			d_energy += prefactor * (cos(new_sum) - cos(old_sum));
+		};
+	auto accumulate_xy = [&](int anchor_field_index, double prefactor)
+		{
+			auto terms = getElectricTerms_xy(anchor_field_index);
+			double old_sum = 0.0, new_sum = 0.0;
+			for (auto term : terms)
+			{
+				old_sum += term.second * site_fields[term.first];
+				new_sum += term.second * site_fields[term.first];
+				if (term.first == index)
+					new_sum += term.second * angle;
+			}
+			d_energy += prefactor * (cos(new_sum) - cos(old_sum));
+		};
+	auto accumulate_b = [&](int anchor_field_index, double prefactor)
+		{
+			auto terms = getMagneticTerms(anchor_field_index);
+			double old_sum = 0.0, new_sum = 0.0;
+			for (auto term : terms)
+			{
+				old_sum += term.second * site_fields[term.first];
+				new_sum += term.second * site_fields[term.first];
+				if (term.first == index)
+					new_sum += term.second * angle;
+			}
+			d_energy += prefactor * (cos(new_sum) - cos(old_sum));
+		};
 
-			if (term.first == index)
-				b_sum_new += term.second * angle;
-		}
-		d_energy += -(cos(b_sum_new) - cos(b_sum_old)) / 2.0;
-	
-		return d_energy;
+	const double e_pref = 1.0 / (2.0 * KU);
+	const double b_pref = -0.5;
+
+	if (type == 0) // A_0(r): appears in E_xx at r, r-x, r-y, r-x-y; in E_xy at r-2x, r-x, r-2y, r-y
+	{
+		accumulate_xx(field_index_from_site(nx, ny, nt, 0), e_pref);
+		accumulate_xx(field_index_from_site(nx_m1, ny, nt, 0), e_pref);
+		accumulate_xx(field_index_from_site(nx, ny_m1, nt, 0), e_pref);
+		accumulate_xx(field_index_from_site(nx_m1, ny_m1, nt, 0), e_pref);
+
+		accumulate_xy(field_index_from_site(nx_m2, ny, nt, 0), e_pref);
+		accumulate_xy(field_index_from_site(nx_m1, ny, nt, 0), e_pref);
+		accumulate_xy(field_index_from_site(nx, ny_m2, nt, 0), e_pref);
+		accumulate_xy(field_index_from_site(nx, ny_m1, nt, 0), e_pref);
 	}
+	else if (type == 1) // A_xx(r): appears in E_xx at r, r-t; in B at r-2x, r-x, r-2y, r-y
+	{
+		accumulate_xx(field_index_from_site(nx, ny, nt, 0), e_pref);
+		accumulate_xx(field_index_from_site(nx, ny, nt_m1, 0), e_pref);
+
+		accumulate_b(field_index_from_site(nx_m2, ny, nt, 0), b_pref);
+		accumulate_b(field_index_from_site(nx_m1, ny, nt, 0), b_pref);
+		accumulate_b(field_index_from_site(nx, ny_m2, nt, 0), b_pref);
+		accumulate_b(field_index_from_site(nx, ny_m1, nt, 0), b_pref);
+	}
+	else // type == 2, A_xy(r): appears in E_xy at r, r-t; in B at r-x-y, r-x, r-y, r
+	{
+		accumulate_xy(field_index_from_site(nx, ny, nt, 0), e_pref);
+		accumulate_xy(field_index_from_site(nx, ny, nt_m1, 0), e_pref);
+
+		accumulate_b(field_index_from_site(nx_m1, ny_m1, nt, 0), b_pref);
+		accumulate_b(field_index_from_site(nx_m1, ny, nt, 0), b_pref);
+		accumulate_b(field_index_from_site(nx, ny_m1, nt, 0), b_pref);
+		accumulate_b(field_index_from_site(nx, ny, nt, 0), b_pref);
+	}
+
+	return d_energy;
 }
 
 double Spiderweb::proposePlaqFlip(int index, double angle) const
@@ -164,7 +219,7 @@ std::vector<std::pair<int, double>> Spiderweb::getElectricTerms_xx(int field_ind
 	electric_terms.push_back({field_index_from_site(nx, ny, (nt + 1) % temporal_size, 1), -1});
 	electric_terms.push_back({field_index_from_site(nx, ny, nt, 1), 1});
 
-	// Q_xy A_00 = 4DxDy A_0 = 4A_0(r + x + y) - 4A_0(r + x) - 4A_0(r + y) + 4A_0(r)
+	// Q_xx A_00 = 4DxDy A_0 = 4A_0(r + x + y) - 4A_0(r + x) - 4A_0(r + y) + 4A_0(r)
 	electric_terms.push_back({ field_index_from_site((nx + 1) % linear_size, (ny + 1) % linear_size, nt, 0), 4 });
 	electric_terms.push_back({ field_index_from_site((nx + 1) % linear_size, ny, nt, 0), -4 });
 	electric_terms.push_back({ field_index_from_site(nx, (ny + 1) % linear_size, nt, 0), -4 });
@@ -192,7 +247,7 @@ std::vector<std::pair<int, double>> Spiderweb::getElectricTerms_xy(int field_ind
 	electric_terms.push_back({field_index_from_site(nx, ny, (nt + 1) % temporal_size, 2), -1});
 	electric_terms.push_back({field_index_from_site(nx, ny, nt, 2), 1});
 
-	// Q_xx A_0 = (DxDx - DyDy)A_0 = A_0(r + 2x) - 2A_0(r + x) - A_0(r + 2y) + 2A_0(r + y)
+	// Q_xy A_0 = (DxDx - DyDy)A_0 = A_0(r + 2x) - 2A_0(r + x) - A_0(r + 2y) + 2A_0(r + y)
 	electric_terms.push_back({ field_index_from_site((nx + 2) % linear_size, ny, nt, 0), 1 });
 	electric_terms.push_back({ field_index_from_site((nx + 1) % linear_size, ny, nt, 0), -2 });
 	electric_terms.push_back({ field_index_from_site(nx, (ny + 2) % linear_size, nt, 0), -1 });
